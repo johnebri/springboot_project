1dd47ed0a4acc65d13868c306add5e108d71b399
johnebri <john.ebri@yahoo.com>
2020-02-09 17:19:14 +0000

    survey controller

---
+++ src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -0,0 +1,25 @@
+package com.in28minutes.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.springboot.model.Question;
+import com.in28minutes.springboot.service.SurveyService;
+
+@RestController
+public class SurveyController {
+	
+	@Autowired
+	private SurveyService surveyService;
+	
+	//  GET/survey/{surveyId}/questions
+	@GetMapping("/surveys/{surveyId}/questions")
+	public List<Question> retrieveQuestionForSurvey(@PathVariable String surveyId) {
+		return surveyService.retrieveQuestions(surveyId);
+	}
+	
+}
