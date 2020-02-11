e8d60626577980ed80adef597201be5dea38b246
johnebri <john.ebri@yahoo.com>
2020-02-11 00:33:22 +0000

    spring dev tools

--- src/main/java/com/in28minutes/springboot/controller/SurveyController.java
+++ src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -22,4 +22,10 @@ public class SurveyController {
 		return surveyService.retrieveQuestions(surveyId);
 	}
 	
+	//   GET /surveys/{surveyId}/questions/{questionId}
+	@GetMapping("/surveys/{surveyId}/questions/{questionId}")
+	public Question retrieveDetailsForQuestion(@PathVariable String surveyId, @PathVariable String questionId) {
+		return surveyService.retrieveQuestion(surveyId, questionId);
+	}
+	
 }
--- src/main/java/com/in28minutes/springboot/service/WelcomeService.java
+++ src/main/java/com/in28minutes/springboot/service/WelcomeService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class WelcomeService{
 	public String retrieveWelcomeMessage() {
 		
-		return "Good Morning updated";
+		return "Good Morning.";
 		
 	}
 	
