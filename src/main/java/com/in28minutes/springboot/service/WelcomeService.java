e87dc7d58e43f7c95f9edf6d37117a372ff1d2ff
johnebri <john.ebri@yahoo.com>
2020-02-15 19:44:03 +0000

    dynamic configuration

--- src/main/java/com/in28minutes/springboot/service/WelcomeService.java
+++ src/main/java/com/in28minutes/springboot/service/WelcomeService.java
@@ -1,12 +1,17 @@
 package com.in28minutes.springboot.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WelcomeService{
+	
+	@Value("${welcome.message}")
+	private String welcomeMessage; 
+	
 	public String retrieveWelcomeMessage() {
 		
-		return "Good Morning. Welcome to Spring";
+		return welcomeMessage;
 		
 	}
 	
